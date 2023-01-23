d3f6eda6e9b8ab43e4211c68bf05b25a0f39488e
João Vitor Marques <jvsilvam@outlook.com>
2023-01-23 20:03:51 -0300

    chore: update eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,10 +15,14 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2021,
     sourceType: 'module',
+    project: ['./tsconfig.json'],
   },
   rules: {
+    'no-undef': 'off',
     'prettier/prettier': ['error'],
+    '@typescript-eslint/no-non-null-assertion': 'off',
     'no-console': 'error',
+    'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     'endOfLine': 'off',
     'prettier/prettier': [
