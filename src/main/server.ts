00a07e51d0413dff1bb9899131f1597fa0c369f2
João Vitor Marques <jvsilvam@outlook.com>
2023-02-09 19:11:31 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,4 @@
-import express from 'express';
-
-const app = express();
+import app from './config/app';
 
 // eslint-disable-next-line no-console
 app.listen(5050, () => console.log('Server running at http://localhost:5050'));
