f66284b061a4ff431de2b31dd26622f28fea9088
João Vitor Marques <jvsilvam@outlook.com>
2023-02-10 09:05:13 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,15 +1,30 @@
 import request from 'supertest';
 import app from '../config/app';
+import { MongoHelper } from '@/src/infra/db/mongodb/helpers/mongo-helper';
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL || '');
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+
+    await accountCollection.deleteMany({});
+  });
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
       .send({
         name: 'any_name',
         email: 'jvsilvam@outlook.com',
-        password: 'password',
-        passwordConfirmation: 'password',
+        password: '1234',
+        passwordConfirmation: '1234',
       })
       .expect(200);
   });
